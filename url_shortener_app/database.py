f8cf8afcdc80c65c7875339028c83eb240db61ee
SmVAwrk <smvawrk@gmail.com>
2021-04-26 17:35:23 +0300

    Initial commit. Подключение БД и ORM

---
+++ url_shortener_app/database.py
@@ -0,0 +1,21 @@
+"""
+Файл подключения ORM к БД
+"""
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# URL БД
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}" \
+                          f"@localhost/short_url_db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Класс для создания сессий БД
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Базовый класс для моделей
+Base = declarative_base
+
